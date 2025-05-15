808a7c6eea9cc1ff778263d8c511ee5567a77684
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-05-15 19:40:11 +0200

    Update companies-view.ts

--- src/frontend/views/companies-view.ts
+++ src/frontend/views/companies-view.ts
@@ -1,37 +1,68 @@
-import { LiteElement, html, css } from '@vandeurenglenn/lite'
+import { LiteElement, html, css, query, property } from '@vandeurenglenn/lite'
 import '@material/web/fab/fab.js'
 import '@vandeurenglenn/lite-elements/icon.js'
-export class CompaniesView extends LiteElement {
+
+import './../flows/data.js'
+import './../flows/data-input.js'
+import './../elements/list/item.js'
+import { CompaniesMixin } from '../mixins/companies.js'
+import '@vandeurenglenn/lite-elements/icon-button.js'
+import './../elements/action/bar.js'
+import './../elements/action/item.js'
+import './../elements/view/header.js'
+
+export class CompaniesView extends CompaniesMixin(LiteElement) {
   static styles = [
     css`
+      :host {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+      }
       md-fab {
         position: fixed;
         bottom: 20px;
         right: 20px;
         z-index: 1000;
       }
-      /* Add your styles here */
+
+      custom-icon-button {
+        min-width: 40px;
+      }
     `
   ]
 
-  addCompany = () => {
-    // Logic to create a new job
-    console.log('Creating a new job...')
-    // You can add your logic here to create a new job
-  }
-
-  _handleFabKeyUp = (event) => {
-    if (event.key === 'Enter' || event.key === 'Space') {
-      event.preventDefault()
-      this.addCompany()
-    }
-  }
-
   render() {
     return html`
+      <view-header
+        title="companies"
+        description="Manage your companies"
+        icon="source_environment"></view-header>
+
+      ${Object.entries(this.companies || {}).map(
+        ([uuid, { name, place }]) => html`
+          <list-item
+            .headline=${name}
+            .subheadline=${place.formattedAddress}
+            .key=${uuid}
+            ><custom-icon-button
+              slot="trailing"
+              icon="delete"
+              @click=${() => {
+                event.stopPropagation()
+                event.preventDefault()
+                event.stopImmediatePropagation()
+                this._deleteCompany(uuid)
+              }}></custom-icon-button
+          ></list-item>
+        `
+      )}
+
       <md-fab
         icon="add"
-        @click=${() => this.addCompany()}
+        @click=${() => this._addCompany()}
         @keyup=${(event) => this._handleFabKeyUp(event)}>
         <custom-icon
           icon="add"
