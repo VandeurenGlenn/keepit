b2c5a303f19911757ca3f922347ef7350116bb44
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-05-15 19:38:38 +0200

    chip-field: handleSelection

--- src/frontend/elements/chip/field.ts
+++ src/frontend/elements/chip/field.ts
@@ -1,8 +1,15 @@
 import { LiteElement, html, css, property } from '@vandeurenglenn/lite'
 import './element.js'
+import { ChipElement } from './element.js'
 
 export class ChipField extends LiteElement {
   @property({ type: Array }) accessor chips
+  @property({ type: Boolean }) accessor customEvent
+  @property({ type: String }) accessor label
+  @property({ type: Boolean, reflect: true }) accessor disabled
+  @property({ type: Boolean, reflect: true }) accessor multi
+  @property({ type: Array }) accessor selected = []
+
   static styles = [
     css`
       :host {
@@ -12,13 +19,29 @@ export class ChipField extends LiteElement {
         width: 100%;
         margin: 12px 0;
         width: 100%;
+        pointer-events: none;
+      }
+
+      .row {
+        display: flex;
+        flex-flow: row wrap;
+        gap: 0.5rem;
+        width: 100%;
       }
     `
   ]
 
   async _addChip() {
+    if (this.customEvent)
+      return this.dispatchEvent(
+        new CustomEvent('add-chip', {
+          bubbles: true,
+          composed: true
+        })
+      )
+
     const label = prompt('Enter a new chip label:')
-    if (!label) return;
+    if (!label) return
 
     const response = await fetch('/api/chips', {
       method: 'POST',
@@ -33,12 +56,57 @@ export class ChipField extends LiteElement {
     }
   }
 
+  firstRender(): void {
+    this.addListener('click', this._handleClick.bind(this))
+    this.addListener('keydown', this._handleKeyDown.bind(this))
+  }
+
+  _handleChipSelected(e: Event) {
+    const el = e.composedPath()[0] as ChipElement
+    if (el.disabled) return
+
+    if (this.multi) {
+      if (!this.selected.includes(el.value)) {
+        // If the chip is not selected, add it to the selected array
+        // Otherwise, remove it from the selected array
+        this.selected.push(el.value)
+      } else {
+        this.selected = this.selected.filter((v) => v !== el.value)
+      }
+      this.requestRender()
+    } else {
+      this.selected = [el.value]
+    }
+
+    this.dispatchEvent(new CustomEvent('chip-selected', { detail: this.selected }))
+  }
+
+  _handleClick(e: CustomEvent) {
+    this._handleChipSelected(e)
+  }
+  _handleKeyDown(e: KeyboardEvent) {
+    if (e.key === 'Enter' || e.key === ' ') {
+      e.preventDefault()
+      this._handleChipSelected(e)
+    }
+  }
+
   render() {
     return html`
-      ${this.chips?.map((label) => html`<chip-element .label=${label}></chip-element>`)}
-      <custom-icon-button
-        @click=${() => this._addChip()}
-        icon="add"></custom-icon-button>
+      ${this.label ? html`<h4>${this.label}</h4>` : ''}
+      <span class="row">
+        ${this.chips?.map(
+          ({ label, value }) =>
+            html`<chip-element
+              ?selected=${this.selected?.includes(value)}
+              .disabled=${this.disabled}
+              .label=${label}
+              .value=${value}></chip-element>`
+        )}
+        <custom-icon-button
+          @click=${() => this._addChip()}
+          icon="add"></custom-icon-button>
+      </span>
     `
   }
 }
