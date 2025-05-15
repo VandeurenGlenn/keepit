329f96feaf0894664f01c4382bd214bfc9a322ba
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-05-15 19:39:25 +0200

    Update icons.ts

--- src/frontend/icons.ts
+++ src/frontend/icons.ts
@@ -8,16 +8,21 @@ export default html` <custom-icon-set>
     <span name="arrow-forward">@symbol-arrow_forward</span>
     <span name="cameraswitch">@symbol-cameraswitch</span>
     <span name="menu">@symbol-menu</span>
+    <span name="edit">@symbol-edit</span>
+    <span name="delete">@symbol-delete</span>
+    <span name="more_vert">@symbol-more_vert</span>
     <span name="home">@symbol-home</span>
     <span name="location_on">@symbol-location_on</span>
     <span name="search">@symbol-search</span>
+    <span name="fact_check">@symbol-fact_check</span>
     <span name="close">@symbol-close</span>
     <span name="check">@symbol-check</span>
-
+    <span name="info">@symbol-info</span>
     <span name="inventory2">@symbol-inventory_2</span>
     <span name="receipt">@symbol-receipt</span>
     <span name="group">@symbol-group</span>
     <span name="save">@symbol-save</span>
     <span name="source_environment">@symbol-source_environment</span>
+    <span name="fact_check">@symbol-fact_check</span>
   </template>
 </custom-icon-set>`
