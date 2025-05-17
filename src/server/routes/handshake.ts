836fe1c35398068b682a410d233b51add86321b0
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-05-17 20:34:35 +0200

    Create handshake.ts

---
+++ src/server/routes/handshake.ts
@@ -0,0 +1,17 @@
+import Router from '@koa/router'
+import { users } from './../database/database.js'
+
+const router = new Router({
+  prefix: '/api/handshake'
+})
+
+router.get('/', async (ctx) => {
+  if (users[ctx.state.userid]) {
+    ctx.body = 'REGISTERED'
+  } else {
+    ctx.body = 'NOT_REGISTERED'
+  }
+  ctx.status = 200
+})
+
+export default router.routes()
