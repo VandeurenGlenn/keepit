524893ab4b2154bb552befc358667af5c8722a10
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-05-31 13:45:51 +0200

    Update job-view.ts

--- src/frontend/views/job-view.ts
+++ src/frontend/views/job-view.ts
@@ -3,6 +3,17 @@ import '@material/web/textfield/outlined-text-field.js'
 import { Job } from '../../types/index.js'
 import './../elements/view/header.js'
 
+function msToTime(ms) {
+  let seconds = (ms / 1000).toFixed(1)
+  let minutes = (ms / (1000 * 60)).toFixed(1)
+  let hours = (ms / (1000 * 60 * 60)).toFixed(1)
+  let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1)
+  if (seconds < 60) return seconds + ' s'
+  else if (minutes < 60) return minutes + 'm'
+  else if (hours < 24) return hours + ' h'
+  else return days + ' Days'
+}
+
 export class JobView extends LiteElement {
   static styles = [
     css`
@@ -37,7 +48,21 @@ export class JobView extends LiteElement {
               @input=${(event: Event) => {
                 const input = event.target as HTMLInputElement
                 this.job.description = input.value
-              }}></md-outlined-text-field>`
+              }}></md-outlined-text-field>
+
+            <h4>hours</h4>
+            ${Object.entries(this.job.hours || {}).map(
+              ([key, value]) => html`
+                <span>
+                  ${key}
+                  <span>
+                    ${Object.entries(value).map(
+                      ([userId, user]) => html` <span> ${userId} ${msToTime(user.checkout - user.checkin)} </span> `
+                    )}
+                  </span>
+                </span>
+              `
+            )}`
         : html`<loading-view></loading-view>`}
     `
   }
