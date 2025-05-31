06b95fda42ada0356e3ab5d8dfcd7cfb45b45f6a
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-05-31 13:44:22 +0200

    Update header.ts

--- src/frontend/elements/view/header.ts
+++ src/frontend/elements/view/header.ts
@@ -4,32 +4,51 @@ export class ViewHeader extends LiteElement {
   @property({ type: String }) accessor title
   @property({ type: String }) accessor description
   @property({ type: String }) accessor icon
+  @property({ type: String, reflect: true }) accessor href
+
   static styles = [
     css`
       :host {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        padding: 16px;
-        box-sizing: border-box;
+        display: block;
         background-color: var(--md-sys-color-tertiary);
         color: var(--md-sys-color-on-tertiary);
         border-radius: var(--md-sys-shape-corner-small);
         --custom-icon-color: var(--md-sys-color-on-tertiary);
         --custom-icon-size: 110px;
         margin: 16px 0;
+
+        width: 100%;
+        padding: 16px;
+        box-sizing: border-box;
+      }
+
+      a {
+        text-decoration: none;
+        color: inherit;
+        cursor: pointer;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        pointer-events: none;
+      }
+
+      :host([href]) a {
+        cursor: pointer;
+        pointer-events: auto;
       }
     `
   ]
   render() {
     return html`
-      <h1>${this.title}</h1>
-      <p>${this.description}</p>
-      <custom-icon
-        .icon=${this.icon}
-        size="large"></custom-icon>
+      <a href=${this.href}>
+        <h1>${this.title}</h1>
+        <p>${this.description}</p>
+        <custom-icon
+          .icon=${this.icon}
+          size="large"></custom-icon>
+      </a>
     `
   }
 }
