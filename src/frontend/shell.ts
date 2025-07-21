93fabe6a41b58ea6f913f5e1b892b8dc166b0021
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-07-21 21:26:16 +0200

    Remove async user status check and handshake fetch
    Refactored checkUserStatus to be synchronous and removed the fetch to /api/handshake. The logic for handling 'NOT_REGISTERED' responses and user registration redirection has been eliminated.

--- src/frontend/shell.ts
+++ src/frontend/shell.ts
@@ -134,7 +134,7 @@ console.log(params);
   }
 
 
-  async checkUserStatus() {
+  checkUserStatus() {
     const token = localStorage.getItem('token')
     if (token) {
       const user = this._decodeToken(token)
@@ -171,15 +171,6 @@ console.log(params);
      
       script.dataset.use_fedcm_for_prompts = 'true'
       document.head.appendChild(script)
-      const response = await fetch('/api/handshake', {
-        method: 'GET'
-      })
-    const data = await response.text()
-    if (data === 'NOT_REGISTERED') {
-      this.userRegistering = true
-      location.hash = '#!/register'
-      return
-    }
     }
   }
 
