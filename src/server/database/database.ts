d1b40f158f57a7dd7babeace9777c32f449553e0
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-07-21 20:38:58 +0200

    Add Prestation type import and hoursStore placeholders
    Imported the Prestation type and added commented placeholders for hoursStore and hours data handling, preparing for future implementation of hours tracking functionality.

--- src/server/database/database.ts
+++ src/server/database/database.ts
@@ -1,4 +1,4 @@
-import { Companies, Invoices, Job, Jobs, Users, BannedUsers } from '../../types/index.js'
+import { Companies, Invoices, Job, Jobs, Users, BannedUsers, Prestation } from '../../types/index.js'
 import { DataStore } from './store.js'
 import { opendir, mkdir } from 'fs/promises'
 
@@ -14,6 +14,7 @@ export const companiesStore = new DataStore('companies')
 export const invoicesStore = new DataStore('invoices')
 export const usersStore = new DataStore('users')
 export const bannedUsersStore = new DataStore('bannedUsers')
+// export const hoursStore = new DataStore('hours')
 
 const year = new Date().getFullYear()
 
@@ -23,11 +24,14 @@ let promises: Promise<any>[] = [
   invoicesStore.get(),
   usersStore.get(),
   bannedUsersStore.get()
+  // hoursStore.get()
 ]
 promises = await Promise.all(promises)
 
 export const jobs = promises[0] as unknown as Jobs
+
 export const companies = promises[1] as unknown as Companies
 export const invoices = promises[2] as unknown as Invoices
 export const users = promises[3] as unknown as Users
 export const bannedUsers = promises[4] as unknown as BannedUsers
+// export const hours = promises[5] as unknown as { [date: string]: Prestation }
